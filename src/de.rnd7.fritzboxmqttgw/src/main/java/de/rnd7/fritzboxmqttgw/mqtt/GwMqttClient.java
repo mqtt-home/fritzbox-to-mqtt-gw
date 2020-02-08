5edbe9542ede5da658fb53f7fd88391598ab5feb
Philipp Arndt <2f.mail@gmx.de>
2020-02-08 13:42:03 +0100

    support retain messages

--- src/de.rnd7.fritzboxmqttgw/src/main/java/de/rnd7/fritzboxmqttgw/mqtt/GwMqttClient.java
+++ src/de.rnd7.fritzboxmqttgw/src/main/java/de/rnd7/fritzboxmqttgw/mqtt/GwMqttClient.java
@@ -63,6 +63,7 @@ public class GwMqttClient {
 				try {
 					final MqttMessage message = new MqttMessage(value.getBytes());
 					message.setQos(QOS);
+					message.setRetained(config.isRetain());
 					mqttClient.publish(topic, message);
 				} catch (final MqttException e) {
 					LOGGER.error(e.getMessage(), e);
